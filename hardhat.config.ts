6c0e6e71c389f18138dee2af82814e9167deca5e
schultztimothy <schultz.timothy52@gmail.com>
2024-01-24 17:46:17 -0700

    feat: IdStakingContract >  Co-authored-by: Lucian @lucianHymer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.23",
 };
 
 export default config;
